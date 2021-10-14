b5a652609dd3e133fb6af8302c257688f41132b7
Gary Guo <gary@garyguo.net>
2021-10-15 00:28:49 +0100

    Add unsafe op in unsafe fn test

---
+++ tests/unsafe_op_in_unsafe_fn.rs
@@ -0,0 +1,17 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+
+use pin_init::*;
+
+#[pin_init]
+struct NeedPin {
+    #[pin]
+    a: usize,
+    #[pin]
+    b: usize,
+}
+
+fn main() {
+    init_stack!(p = specify_err::<_, (), _>(NeedPin { a: 1, b: 2 }));
+    let p = p.unwrap();
+    let _ = p;
+}
